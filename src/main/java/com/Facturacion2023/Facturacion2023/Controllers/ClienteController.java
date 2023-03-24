a4b905b48de63ec78c29a75da61d0bb1127bb4dd
salvarenga25 <71409222+salvarenga25@users.noreply.github.com>
2023-03-24 13:36:50 -0400

    Updated version from March 16th, 04:26 P.M.

--- src/main/java/com/Facturacion2023/Facturacion2023/Controllers/ClienteController.java
+++ src/main/java/com/Facturacion2023/Facturacion2023/Controllers/ClienteController.java
@@ -19,6 +19,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/cliente")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class ClienteController {
     @Autowired
     ClienteRepository clienteRepository;
